e0e0bca8086fa2c67ad66339258622744e8cb8bf
shenwei12303 <shenwei12303@outlook.com>
2019-03-21 21:17:57 +0800

    C3

--- main.go
+++ main.go
@@ -3,13 +3,55 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	// http.HandleFunc("/", handlerFunc)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	http.ListenAndServe(":3000", r)
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch, please send an email "+
+			"to <a href=\"mailto:shenwei12303@outlook.com\">"+
+			"shenwei12303@outlook.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>We could not find the page you "+
+			"were looking for :(</h1>"+
+			"<p>Please email us if you keep being sent to an "+
+			"invalid page.</p>")
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:shenwei12303@outlook.com\">"+
+		"shenwei12303@outlook.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>FAQ PAGE</h1>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request)  {
+	w.WriteHeader(404)
+	fmt.Fprintf(w, "<h1>404 NOT FOUND</h1>")
 }
