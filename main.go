e85e6c1943af2f1e94b5bc99617e936ba8b89264
shenwei12303 <shenwei12303@outlook.com>
2019-03-21 18:31:32 +0800

    C2

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
